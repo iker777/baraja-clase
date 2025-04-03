aee069125d99203712837eb7ccd1cccaaf501abc
iker777 <ikeralvarez@gmail.com>
2025-04-03 18:59:41 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// 1. Crear una carta por cada alumno
+// 2. elegir una carta cuando le damos al botón
+// 3. Que haga una animación al elegir la carta. Una cuenta atrás: 3, 2, 1 y el nombre del elegido
+// 4. Tener un apartado para cambiar el número de alumnos que hay en clase
